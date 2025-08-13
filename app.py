936b998c079efbfba382bac147279527b3526748
Nathan <nathanz@native.com>
2025-08-13 10:53:33 +0100

    changing port for render

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from routes import qdrant_bp
+import os
 
 app = Flask(__name__)
 app.register_blueprint(qdrant_bp)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
